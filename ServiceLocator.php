92b7c0d6bd941280b5ac63bbfb047c3cd5005c91
denis909 <denis909@mail.ru>
2019-09-28 09:34:47 +0300

    n/a

---
+++ ServiceLocator.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author denis909
+ * @license MIT
+ */
+namespace denis909\ServiceLocator;
+
+use Closure;
+
+abstract class ServiceLocator
+{
+
+    protected static $_services = [];
+
+    protected static function getService($name, $class, array $params = [])
+    {
+        if (array_key_exists($name, static::$_services))
+        {
+            return static::$_services[$name];
+        }
+
+        if ($class instanceof Closure)
+        {
+            static::$_services[$name] = $class();
+        }
+        elseif(is_object($class))
+        {
+            static::$_services[$name] = $class;
+        }
+        else
+        {
+            static::$_services[$name] = new $class(...$params);
+        }
+        
+        return static::$_services[$name];
+    }
+
+}
